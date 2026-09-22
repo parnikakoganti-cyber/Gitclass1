3d2b5613e79761cf7f20d9fb4aee558089597ed7
Parnika <parnika@gmail.com>
2026-09-22 10:37:44 +0530

    week-7

---
+++ New folder/javascript intro.js
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <h2 id="demo">Hello World!</h2>
+    <button onclick="changeText()">Click Me!</button>
+    <script>
+        function ChangeText() {
+            document.getElementById("demo").innerHTML = "Welcome to java script!";
+        }
+    </script>
+</body>
+</html>
